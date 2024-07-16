6f1f58bd554fbfa8e4514c62e621b297a2de906f
ivan <ivan.k@track160.com>
2024-07-16 19:43:29 +0300

    dark mode for life!

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,24 @@
-import { StrictMode } from 'react';
+import {StrictMode} from 'react';
 import * as ReactDOM from 'react-dom/client';
 import {App} from "./app/App";
 import './main.css'
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App/>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <App/>
+    </ThemeProvider>
   </StrictMode>
 );
