aa295b67ca4d053c13c1c171703a1013236c02bc
ivan <ivan.k@track160.com>
2024-07-16 18:18:07 +0300

    basic api added

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,16 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import axios from "axios";
 
 const PORT = 8787
 
 const app = new Hono()
-app.get('/', (c) => c.text('Hello Node.js!'))
+app.get('/search', async (c) => {
+  const { q } = c.req.query()
+  const url = `http://api.duckduckgo.com/?q=${q}&format=json`
+  const res = await axios.get(url)
+  return c.json(res.data)
+})
 
 console.log(`Server running at http://localhost:${PORT}`)
 serve({
