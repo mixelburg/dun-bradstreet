beaf0476ffad648fc19598bb1ebb05f27a60e6f8
ivan <ivan.k@track160.com>
2024-07-16 18:15:15 +0300

    init

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const PORT = 8787
+
+const app = new Hono()
+app.get('/', (c) => c.text('Hello Node.js!'))
+
+console.log(`Server running at http://localhost:${PORT}`)
+serve({
+  fetch: app.fetch,
+  port: PORT,
+})
